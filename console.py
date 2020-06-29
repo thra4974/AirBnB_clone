e2d94be86022dce04906561454f8593b48bc7ace
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-29 10:27:28 -0500

    added quit, EOF methods to HBHN command class in console.py

--- console.py
+++ console.py
@@ -5,4 +5,18 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """ defines class HBNBCommand """
-    
+    intro = 'Welcome to HBNB command. Type help or ? for options. \n'
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ implements quit command """
+        print("hasta la fuego")
+        return True
+
+    def do_EOF(self, arg):
+        """ implements EOF """
+        return True
+
+if __name__ == '__main__':
+    H_prompt = HBNBCommand()
+    H_prompt.cmdloop()
